d5f4c8e476a2bf5093f1ee7efcb4e965eedfba6b
Kevin Lin <klin50@stuy.edu>
2024-12-02 15:35:16 -0500

    main

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
+#include "parse.h"
 
 int main(){
   char buffer[256];
   fgets(buffer,255,stdin);
-  printf("%s\n",buffer);
+  char* args[16];
+  parse_args(buffer,args);
+  execvp(args[0],args);
   return 0;
 }
---
+++ parse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+void parse_args( char * line, char ** arg_ary ){
+  int i = 0;
+  while((arg_ary[i] = strsep(&line, " "))){
+    i++
+  }
+  arg_ary[i] = NULL;
+}
