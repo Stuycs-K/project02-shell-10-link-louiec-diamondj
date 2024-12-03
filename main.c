ba267f292b813efa9cc8bcd8540ba90f67c4a7f4
Julien Diamond <jdiamond50@stuy.edu>
2024-12-03 14:33:44 -0500

    main now forks() and uses execvp to run command in a while(1) loop

--- main.c
+++ main.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include "parse.h"
 
 int main(){
   char buffer[256];
-  fgets(buffer,255,stdin);
-  char* args[16];
-  parse_args(buffer,args);
-  execvp(args[0],args);
+  while (1) {
+    fgets(buffer,255,stdin);
+    char* args[16];
+    parse_args(buffer,args);
+    pid_t p = fork();
+    if (p < 0) {
+      perror("fork failed\n");
+    } else if (p == 0) {
+      execvp(args[0], args);
+    } else {
+      int status;
+      wait(&status);
+    }
+  }
   return 0;
 }
--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 compile: main.o parse.o
-	gcc -o shell main.o parse.o
+	@gcc -o shell main.o parse.o
 main.o: main.c
-	gcc -c main.c
+	@gcc -c main.c
 parse.o: parse.c parse.h
-	gcc -c parse.c parse.h
+	@gcc -c parse.c parse.h
 clean:
 	rm *.o
+	rm *.gch
 	rm shell
