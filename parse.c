b8e91c64c85c7de6960ebec1d505fd45d6d07e12
jdiamond50 <144475375+jdiamond50@users.noreply.github.com>
2024-12-09 06:57:10 -0500

    added function descriptions to parse.c

--- parse.c
+++ parse.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <string.h>
 
+// takes string of potentially multiple commands and a char** to sort the commands
+// each command (separated by semicolons) is put into a unique index of the char** cmd_ary
 void parse_cmds(char* line, char** cmd_ary) {
   int i = 0;
   while ((cmd_ary[i] = strsep(&line, ";"))) {
@@ -13,6 +15,8 @@ void parse_cmds(char* line, char** cmd_ary) {
   cmd_ary[i] = NULL;
 }
 
+// takes string of potentially multiple arguments in one command and a char** to sort the arguments
+// each argument (separated by spaces) is put into a unique index of the char** arg_ary
 void parse_args(char* line, char** arg_ary){
   int i = 0;
   while((arg_ary[i] = strsep(&line, " "))){
